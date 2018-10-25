ced2d89bfa70873ac9cf778eba599802a176c7c9
jkarik <patrikariktwix@gmail.com>
2018-10-25 14:24:50 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    // document.getElementById("show-menu").style.display = "block";
+    document.getElementById("show-menu").style.opacity = "1";
+    document.getElementById("show-menu").style.left = "0";
+}
+
+function closeNav() {
+    // document.getElementById("show-menu").style.display = "none";
+    document.getElementById("show-menu").style.opacity = "0";
+    document.getElementById("show-menu").style.left = "-120vw";
+}
